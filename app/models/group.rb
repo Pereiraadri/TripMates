2c64d26231eb6a71ef142833f4de8aa098bc109a
Sami Chabane <chabanesami7@gmail.com>
2024-11-27 17:27:27 +0100

    create polls

--- app/models/group.rb
+++ app/models/group.rb
@@ -6,10 +6,16 @@ class Group < ApplicationRecord
   has_one_attached :cover_banner
 
   before_create :generate_invite_token
+  after_create :create_polls
 
-def generate_invite_token
-  self.invite_token = SecureRandom.hex(10)
-end
+  def generate_invite_token
+    self.invite_token = SecureRandom.hex(10)
+  end
 
+  def create_polls
+    Poll.create(category: 0, group: self)
+    Poll.create(category: 10, group: self)
+    Poll.create(category: 20, group: self)
+  end
 
 end
