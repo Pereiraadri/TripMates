059a0bf4f15286af84fb01c9c7dd8217520cca86
Sami Chabane <chabanesami7@gmail.com>
2024-11-27 16:31:57 +0100

    pollcontrollers, ajout des boutons dans la show du groupe

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :owned_groups, class_name: "Group", foreign_key: :owner_id, inverse_of: :owner, dependent: :destroy
   has_many :votes
   has_many :choices
+  has_one_attached :avatar
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,8 @@ p"Creating user"
 user = User.create!(email: "email@gmail.com", password: "123456")
 
 p "Creating group"
-group = Group.create!(title: "villa fin de saison", description: "recherche d'une villa pour la fin de saison", cover_banner: "https://tcsvoyages.ch/wp-content/uploads/2020/03/photo-titre-ou-partir-en-ete_pixabay-1620x1080.jpg", owner: user)
+group = Group.create!(title: "villa fin de saison", description: "recherche d'une villa pour la fin de saison", owner: user)
+#group = Group.create!(title: "villa fin de saison", description: "recherche d'une villa pour la fin de saison", cover_banner: "https://tcsvoyages.ch/wp-content/uploads/2020/03/photo-titre-ou-partir-en-ete_pixabay-1620x1080.jpg", owner: user)
 
 p "Creating poll"
 poll_budget = Poll.create!(title: "test budget", category: 0, state: 0, group: group)
