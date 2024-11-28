1de34ffc2cdad8ae2ca6d7b340c4ad7c0264fda1
adrien pereira <pereiraadrien3@gmail.com>
2024-11-28 16:10:58 +0100

    amelioration sondage

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,9 @@
 class VotesController < ApplicationController
   def create
-    @choice = Choice.find(params[:choice_id])
+    @choice = Choice.find_by(id: vote_params[:choice_id])
+
+    redirect_to root_path, alert: "Choice not found!" and return if @choice.nil?
+
     @vote = current_user.votes.build(choice: @choice)
 
     if @vote.save
@@ -8,5 +11,11 @@ class VotesController < ApplicationController
     else
       redirect_to poll_path(@choice.poll), alert: "You have already voted!"
     end
+  end
+
+  private
+
+  def vote_params
+    params.require(:vote).permit(:choice_id)
   end
 end
