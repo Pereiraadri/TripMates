f5862d4acc48100c51cc1388458b911fcc4fe558
Julia Vitu <juliavitu@gmail.com>
2024-12-02 17:22:20 +0100

    show-polls-in-group

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -2,4 +2,8 @@ class Choice < ApplicationRecord
   belongs_to :poll
   belongs_to :user
   has_many :votes, dependent: :destroy
+
+  def percentage
+    (votes.count.to_f / poll.votes.count * 100).round(2)
+  end
 end
