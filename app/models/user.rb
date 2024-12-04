d0b94bc5d056088171ce01798bdf01e27d2aa376
adrien pereira <pereiraadrien3@gmail.com>
2024-12-04 11:11:32 +0100

    fix affichage avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :user_groups
   has_many :groups, through: :user_groups
   has_many :owned_groups, class_name: "Group", foreign_key: :owner_id, inverse_of: :owner, dependent: :destroy
-  has_many :votes
+  has_many :votes, dependent: :destroy
   has_many :choices
   has_one_attached :avatar
 
